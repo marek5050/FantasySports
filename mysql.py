36dc7840c0f9b7d30055c5bdee2b0c6bef78fd8d
Marek <marek.bejda@gmail.com>
2017-11-25 07:18:14 +0000

    extras

--- mysql.py
+++ mysql.py
@@ -12,7 +12,7 @@ from sqlalchemy.sql import select
 
 settings = {
     "mysql_user": "root",
-    "mysql_pwd": os.getenv('MYSQL_PASSWORD', ""),
+    "mysql_pwd": os.getenv('MYSQL_PASSWORD', "snowDIhawk123$$!"),
     "mysql_host":"127.0.0.1",
     "mysql_db": "fantasy"
 }
